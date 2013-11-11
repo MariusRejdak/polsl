327964070c868a4cd4a7db3052c80d0668892a48
Marius Rejdak <mariuswol@gmail.com>
2013-11-11 16:53:46 +0100

    ZBP Lab2 zad1

--- ZBP/lab2/zad1.cpp
+++ ZBP/lab2/zad1.cpp
@@ -5,54 +5,65 @@
 
 Running:
 #!/bin/fish
-g++ -Wall -std=c++11 -o3 -o file1 file1.cpp; and ./file1
+g++ -Wall -std=c++11 -o3 -o file1 zad1.cpp; and ./zad1
+
+Output:
+English to std::set 0.080332s
+Polish to std::set 2.55033s
+English to std::unordered_set 0.020802s
+Polish to std::unordered_set 0.530011s
+English searching in std::set 0.067151s
+Polish searching in std::set 2.33132s
+English searching in std::unordered_set 0.015167s
+Polish searching in std::unordered_set 0.42904s
+Polish searching in std::set for ratio 0.05 0.112123s
+Polish searching in std::unordered_set for ratio 0.05 0.022894s
+Polish searching in std::set for ratio 0.1 0.22494s
+Polish searching in std::unordered_set for ratio 0.1 0.046557s
+Polish searching in std::set for ratio 0.2 0.449548s
+Polish searching in std::unordered_set for ratio 0.2 0.089799s
+Polish searching in std::set for ratio 0.4 0.907567s
+Polish searching in std::unordered_set for ratio 0.4 0.180477s
+Polish searching in std::set for ratio 0.7 1.60857s
+Polish searching in std::unordered_set for ratio 0.7 0.310871s
+Polish searching in std::set for ratio 1 2.31883s
+Polish searching in std::unordered_set for ratio 1 0.429663s
 
 */
 
 #include <iostream>     // std::cout
 #include <fstream>		// std::ifstream
-#//include <functional>	// std::function
 #include <iterator>		// std::istream_iterator
 #include <algorithm>    // std::sort, std::random_shuffle
 #include <vector>       // std::vector
 #include <string>		// std::string
 #include <ctime>        // time, clock, clock_t, CLOCKS_PER_SEC
 #include <cmath>		// std::round
-//#include <cstdlib>      // srand, qsort
-//#include <list>			// std::list
 #include <set>			// std::set
 #include <unordered_set> // std::unordered_set
 
 //using namespace std; // http://stackoverflow.com/questions/1452721/why-is-using-namespace-std-considered-bad-practice
 
-//template <class T>
-//float get_time(T& container, std::function<void(T&)> sorting_function) {
-//	clock_t clockStart = clock();
-//	sorting_function(container);
-//	return ((float)clock()-clockStart)/CLOCKS_PER_SEC;
-//}
-
-template <class E, class C, class S>
-void insert_vector_to_set(C &cont, S &set)
+template <class AssocContainer, class ForwardIterator>
+float insert_time(AssocContainer &container, ForwardIterator iter_begin, ForwardIterator iter_end)
 {
-	for(E item : cont)
-	{
-		set.insert(item);
-	}
+	clock_t clockStart = clock();
+	container.insert(iter_begin, iter_end);
+	return ((float)clock()-clockStart)/CLOCKS_PER_SEC;
 }
 
-template <class E, class C, class S>
-void test_searching_in_set(C &cont, S &set)
+template <class AssocContainer, class Container>
+float search_time(AssocContainer &container, Container &search_items)
 {
-	for(E item : cont)
+	clock_t clockStart = clock();
+	for(auto item : search_items)
 	{
-		set.find(item);
+		container.find(item);
 	}
+	return ((float)clock()-clockStart)/CLOCKS_PER_SEC;
 }
 
 int main(int argc, char *argv[]) {
-	clock_t clockStart;
-
 	std::ifstream in_english("english.dic", std::ifstream::in);
 	std::ifstream in_polish("polish.dic", std::ifstream::in);
 	std::istream_iterator<std::string> in_iter_english(in_english), in_iter_polish(in_polish), eos;
@@ -63,66 +74,28 @@ int main(int argc, char *argv[]) {
 	std::random_shuffle(lines_english.begin(), lines_english.end());
 	std::random_shuffle(lines_polish.begin(), lines_polish.end());
 
-	std::vector<std::string> test_lines_english(lines_english.begin(), lines_english.begin()+10000);
-	std::vector<std::string> test_lines_polish(lines_polish.begin(), lines_polish.begin()+10000);
-
 	std::set<std::string> set_polish, set_english;
 	std::unordered_set<std::string> uset_polish, uset_english;
-
 	
-	clockStart = clock();
-	insert_vector_to_set<std::string>(lines_english, set_english);
-	std::cout << "English to std::set " << ((float)clock()-clockStart)/CLOCKS_PER_SEC << "s\n";
+	std::cout << "English to std::set " << insert_time(set_english, lines_english.begin(), lines_english.end()) << "s\n";
+	std::cout << "Polish to std::set " << insert_time(set_polish, lines_polish.begin(), lines_polish.end()) << "s\n";
+	std::cout << "English to std::unordered_set " << insert_time(uset_english, lines_english.begin(), lines_english.end()) << "s\n";
+	std::cout << "Polish to std::unordered_set " << insert_time(uset_polish, lines_polish.begin(), lines_polish.end()) << "s\n";
 
-	clockStart = clock();
-	insert_vector_to_set<std::string>(lines_polish, set_polish);
-	std::cout << "Polish to std::set " << ((float)clock()-clockStart)/CLOCKS_PER_SEC << "s\n";
+	std::cout << "English searching in std::set " << search_time(set_english, lines_english) << "s\n";
+	std::cout << "Polish searching in std::set " << search_time(set_polish, lines_polish) << "s\n";
+	std::cout << "English searching in std::unordered_set " << search_time(uset_english, lines_english) << "s\n";
+	std::cout << "Polish searching in std::unordered_set " << search_time(uset_polish, lines_polish) << "s\n";
 
-	clockStart = clock();
-	insert_vector_to_set<std::string>(lines_english, uset_english);
-	std::cout << "English to std::unordered_set " << ((float)clock()-clockStart)/CLOCKS_PER_SEC << "s\n";
-
-	clockStart = clock();
-	insert_vector_to_set<std::string>(lines_polish, uset_polish);
-	std::cout << "Polish to std::unordered_set " << ((float)clock()-clockStart)/CLOCKS_PER_SEC << "s\n";
-
-	clockStart = clock();
-	test_searching_in_set<std::string>(test_lines_english, set_english);
-	std::cout << "English searching in std::set " << ((float)clock()-clockStart)/CLOCKS_PER_SEC << "s\n";
-
-	clockStart = clock();
-	test_searching_in_set<std::string>(test_lines_polish, set_polish);
-	std::cout << "Polish searching in std::set " << ((float)clock()-clockStart)/CLOCKS_PER_SEC << "s\n";
-
-	clockStart = clock();
-	test_searching_in_set<std::string>(test_lines_english, uset_english);
-	std::cout << "English searching in std::unordered_set " << ((float)clock()-clockStart)/CLOCKS_PER_SEC << "s\n";
-
-	clockStart = clock();
-	test_searching_in_set<std::string>(test_lines_polish, uset_polish);
-	std::cout << "Polish searching in std::unordered_set " << ((float)clock()-clockStart)/CLOCKS_PER_SEC << "s\n";
-	
-
-	float slices[] = { 0.05, 0.1, 0.2, 0.4, 0.7, 1};
-	test_lines_polish.clear();
-	test_lines_polish.insert(test_lines_polish.end(), lines_polish.begin(), lines_polish.begin()+(int)round(lines_polish.size()*slices[0]));
+	float slices[] = {0.05, 0.1, 0.2, 0.4, 0.7, 1};
+	std::vector<std::string> test_lines;
 
 	for(float ratio : slices)
 	{
-		set_polish.clear();
-		set_polish.insert(lines_polish.begin(), lines_polish.begin()+(int)round(lines_polish.size()*ratio));
-		clockStart = clock();
-		test_searching_in_set<std::string>(test_lines_polish, set_polish);
-		std::cout << "Polish searching in std::set for ratio " << ratio << " " << ((float)clock()-clockStart)/CLOCKS_PER_SEC << "s\n";
-	}
-
-	for(float ratio : slices)
-	{
-		uset_polish.clear();
-		uset_polish.insert(lines_polish.begin(), lines_polish.begin()+(int)round(lines_polish.size()*ratio));
-		clockStart = clock();
-		test_searching_in_set<std::string>(test_lines_polish, uset_polish);
-		std::cout << "Polish searching in std::unordered_set for ratio " << ratio << " " << ((float)clock()-clockStart)/CLOCKS_PER_SEC << "s\n";
+		test_lines.insert(test_lines.end(), lines_polish.begin(), lines_polish.begin()+(int)round(lines_polish.size()*ratio));
+		std::cout << "Polish searching in std::set for ratio " << ratio << " " << search_time(set_polish, test_lines) << "s\n";
+		std::cout << "Polish searching in std::unordered_set for ratio " << ratio << " " << search_time(uset_polish, test_lines) << "s\n";
+		test_lines.clear();
 	}
 
 	return 0; //Huge success!
