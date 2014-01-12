47f184234f223100840e7d184effa740c3af8d56
Marius Rejdak <mariuswol@gmail.com>
2014-01-12 18:54:16 +0100

    ZBP Makefile fixes

--- ZBP/projekt/Graph.h
+++ ZBP/projekt/Graph.h
@@ -6,12 +6,12 @@
 
 class Graph {
 public:
-    Graph(const int *matrix, size_t size);
-    Graph(const std::vector<std::vector<int> > &matrix);
-    std::vector<int> dijkstra(unsigned int start_vertex);
-    std::vector<std::vector<int> > floyd_warshall();
-    std::vector<int> bfs(unsigned int start_vertex);
-    std::vector<int> dfs(unsigned int start_vertex);
+	Graph(const int *matrix, size_t size);
+	Graph(const std::vector<std::vector<int> > &matrix);
+	std::vector<int> dijkstra(unsigned int start_vertex);
+	std::vector<std::vector<int> > floyd_warshall();
+	std::vector<int> bfs(unsigned int start_vertex);
+	std::vector<int> dfs(unsigned int start_vertex);
 private:
 	std::vector<std::vector<int> > matrix;
 	size_t m_size;
--- ZBP/projekt/main.cpp
+++ ZBP/projekt/main.cpp
@@ -9,48 +9,48 @@ typedef std::vector<std::vector<int> > matrix_t;
 
 matrix_t generate_graph(size_t size, int max_edges, int max_val)
 {
-    matrix_t matrix(size, std::vector<int>(size));
+	matrix_t matrix(size, std::vector<int>(size));
 
-    for(size_t i = 0; i < size; ++i)
-    {
-        for(size_t j = 0; j < size; ++j)
-        {
-            matrix[i][j] = i == j ? 0 : rand()%max_val;
-        }
-    }
+	for(size_t i = 0; i < size; ++i)
+	{
+		for(size_t j = 0; j < size; ++j)
+		{
+			matrix[i][j] = i == j ? 0 : rand()%max_val;
+		}
+	}
 
-    int edges = size*(size-1);
-    while(edges > max_edges)
-    {
-        int i = rand()%size;
-        int j = rand()%size;
+	int edges = size*(size-1);
+	while(edges > max_edges)
+	{
+		int i = rand()%size;
+		int j = rand()%size;
 
-        if(i != j && matrix[i][j] != -1)
-        {
-            matrix[i][j] = -1;
-            --edges;
-        }
-    }
+		if(i != j && matrix[i][j] != -1)
+		{
+			matrix[i][j] = -1;
+			--edges;
+		}
+	}
 
-    return matrix;
+	return matrix;
 }
 
 int main()
 {
-    matrix_t m1 = generate_graph(1000, 100000, 30);
+	matrix_t m1 = generate_graph(1000, 100000, 30);
 
-    Graph g(m1);
-    BoostGraphWrapper bg(m1);
+	Graph g(m1);
+	BoostGraphWrapper bg(m1);
 
-    clock_t clockStart = clock();
-    g.dijkstra(0);
-    float time_g1 = ((float)clock()-clockStart)/CLOCKS_PER_SEC;
-    std::cout << time_g1 << "\n";
+	clock_t clockStart = clock();
+	g.dijkstra(0);
+	float time_g1 = ((float)clock()-clockStart)/CLOCKS_PER_SEC;
+	std::cout << time_g1 << "\n";
 
-    clockStart = clock();
-    bg.dijkstra(0);
-    float time_g2 = ((float)clock()-clockStart)/CLOCKS_PER_SEC;
-    std::cout << time_g2 << "\n";
+	clockStart = clock();
+	bg.dijkstra(0);
+	float time_g2 = ((float)clock()-clockStart)/CLOCKS_PER_SEC;
+	std::cout << time_g2 << "\n";
 
-    return 0;
+	return 0;
 }
