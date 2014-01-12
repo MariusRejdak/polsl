9188793cee671a6443eec4a5ad2e631211769c70
Marius Rejdak <mariuswol@gmail.com>
2014-01-13 00:27:47 +0100

    ZBP Final

--- ZBP/projekt/main.cpp
+++ ZBP/projekt/main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <ctime>
+#include <memory>
 
 #include "Graph.h"
 #include "BoostGraphWrapper.h"
 
 typedef std::vector<std::vector<int> > matrix_t;
 
-matrix_t generate_graph(size_t size, int max_edges, int max_val)
+matrix_t generate_graph(size_t size, int edges, int max_val)
 {
 	matrix_t matrix(size, std::vector<int>(size));
 
@@ -19,8 +20,8 @@ matrix_t generate_graph(size_t size, int max_edges, int max_val)
 		}
 	}
 
-	int edges = size*(size-1);
-	while(edges > max_edges)
+	int now_edges = size*(size-1);
+	while(now_edges > edges)
 	{
 		int i = rand()%size;
 		int j = rand()%size;
@@ -28,29 +29,63 @@ matrix_t generate_graph(size_t size, int max_edges, int max_val)
 		if(i != j && matrix[i][j] != -1)
 		{
 			matrix[i][j] = -1;
-			--edges;
+			--now_edges;
 		}
 	}
 
 	return matrix;
 }
 
+float get_time(std::function<void()> function) {
+	clock_t clockStart = clock();
+	function();
+	return ((float)clock()-clockStart)/CLOCKS_PER_SEC;
+}
+
+typedef struct Graph_Details
+{
+	size_t size;
+	int edges;
+	int max_val;
+} Graph_Details;
+
+void test_graph(Graph_Details &gd)
+{
+	matrix_t m = generate_graph(gd.size, gd.edges, gd.max_val);
+	Graph mg(m);
+	BoostGraphWrapper bg(m);
+	int start_vertex = rand()%gd.size;
+
+	std::cout << gd.size << ";" << gd.edges << ";";
+	std::cout << get_time([&]() -> void {mg.dijkstra(start_vertex);} ) << ";";
+	std::cout << get_time([&]() -> void {bg.dijkstra(start_vertex);} ) << ";";
+	std::cout << get_time([&]() -> void {mg.floyd_warshall();} ) << ";";
+	std::cout << get_time([&]() -> void {bg.floyd_warshall();} ) << ";";
+	std::cout << get_time([&]() -> void {mg.bfs(start_vertex);} ) << ";";
+	std::cout << get_time([&]() -> void {bg.bfs(start_vertex);} ) << ";";
+	std::cout << get_time([&]() -> void {mg.dfs(start_vertex);} ) << ";";
+	std::cout << get_time([&]() -> void {bg.dfs(start_vertex);} );
+	std::cout << std::endl;
+}
+
 int main()
 {
-	matrix_t m1 = generate_graph(1000, 100000, 30);
+	Graph_Details test_cases[] = {
+		{ 10, 10, 50 }
+	,	{ 10, 100, 50 }
+	,	{ 100, 100, 50 }
+	,	{ 100, 10000, 50 }
+	,	{ 1000, 1000, 100 }
+	,	{ 1000, 10000, 100 }
+	,	{ 1000, 50000, 100 }
+	,	{ 1000, 100000, 100 }
+	,	{ 1000, 500000, 100 }
+	,	{ 1000, 1000000, 100 }
+	};
 
-	Graph g(m1);
-	BoostGraphWrapper bg(m1);
-
-	clock_t clockStart = clock();
-	g.dijkstra(0);
-	float time_g1 = ((float)clock()-clockStart)/CLOCKS_PER_SEC;
-	std::cout << time_g1 << "\n";
-
-	clockStart = clock();
-	bg.dijkstra(0);
-	float time_g2 = ((float)clock()-clockStart)/CLOCKS_PER_SEC;
-	std::cout << time_g2 << "\n";
+	std::cout << "vertices;edges,dijkstra_time;dijkstra_boost_time;floyd_warshall_time;floyd_warshall_boost_time;bfs_time;bfs_boost_time;dfs_time;dfs_boost_time" << std::endl;
+	for (auto gd : test_cases)
+		test_graph(gd);
 
 	return 0;
 }
