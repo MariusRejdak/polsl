3d30e4b54e2b6073e18e61411194aa28f77f67d0
Marius Rejdak <mariuswol@gmail.com>
2014-01-12 01:16:31 +0100

    ZBP WTF DFS, why U not work?!

--- ZBP/projekt/main_boost.cpp
+++ ZBP/projekt/main_boost.cpp
@@ -1,12 +1,7 @@
-//=======================================================================
-// Copyright 2001 Jeremy G. Siek, Andrew Lumsdaine, Lie-Quan Lee, 
-//
-// Distributed under the Boost Software License, Version 1.0. (See
-// accompanying file LICENSE_1_0.txt or copy at
-// http://www.boost.org/LICENSE_1_0.txt)
-//=======================================================================
 #include <boost/config.hpp>
 #include <iostream>
+
+#include <vector>
 
 #include <boost/graph/graph_traits.hpp>
 #include <boost/graph/adjacency_list.hpp>
@@ -15,9 +10,11 @@
 #include <boost/graph/exterior_property.hpp>
 #include <boost/graph/floyd_warshall_shortest.hpp>
 
+#include <boost/graph/properties.hpp>
 #include <boost/graph/breadth_first_search.hpp>
-
 #include <boost/graph/depth_first_search.hpp>
+
+#include <boost/graph/graphviz.hpp>
 
 using namespace boost;
 
@@ -26,7 +23,13 @@ main(int, char *[])
 {
   typedef adjacency_list_traits<listS, listS, directedS>::vertex_descriptor vertex_descriptor;
   typedef adjacency_list < listS, listS, directedS,
-    property<vertex_index_t, int, property<vertex_distance_t, float, property<vertex_predecessor_t, vertex_descriptor> > >, 
+    property<vertex_index_t, int, 
+      property<vertex_distance_t, float, 
+        property<vertex_color_t, default_color_type,
+          property<vertex_predecessor_t, vertex_descriptor>
+        > 
+      >
+    >, 
     property<edge_weight_t, float> 
   > graph_t;
   typedef std::pair<int, int> Edge;
@@ -37,10 +40,10 @@ main(int, char *[])
 
   const int num_nodes = 5;
   enum nodes { A, B, C, D, E };
-  Edge edge_array[] = { Edge(A, C), Edge(B, B), Edge(B, D), Edge(B, E),
+  Edge edge_array[] = { Edge(B, B), Edge(B, D), Edge(B, E),
     Edge(C, B), Edge(C, D), Edge(D, E), Edge(E, A), Edge(E, B)
   };
-  float weights[] = { 1, 2, 1, 2, 7, 3, 1, 1, 1 };
+  float weights[] = { 2, 1, 2, 7, 3, 1, 1, 1 };
   int num_arcs = sizeof(edge_array) / sizeof(Edge);
   graph_traits<graph_t>::vertex_iterator i, iend;
 
@@ -49,41 +52,54 @@ main(int, char *[])
   // Manually intialize the vertex index and name maps
   property_map<graph_t, vertex_index_t>::type indexmap = get(vertex_index, g);
   int c = 0;
-  for (boost::tie(i, iend) = vertices(g); i != iend; ++i, ++c) {
+  for (tie(i, iend) = vertices(g); i != iend; ++i, ++c) {
     indexmap[*i] = c;
   }
 
+  graph_traits <graph_t>::vertex_iterator vi, vend;
+
   vertex_descriptor s = vertex(A, g);
+
+  write_graphviz(std::cout, g);
 
   property_map<graph_t, vertex_distance_t>::type d = get(vertex_distance, g);
   property_map<graph_t, vertex_predecessor_t>::type p = get(vertex_predecessor, g);
-  dijkstra_shortest_paths(g, s, predecessor_map(p).distance_map(d));
+  property_map<graph_t, vertex_color_t>::type cm = get(vertex_color, g);
+
+  /*dijkstra_shortest_paths(g, s, predecessor_map(p).distance_map(d));
 
   distance_matrix_t distances(num_vertices(g));
   distance_matrix_map_t dm(distances, g);
   floyd_warshall_all_pairs_shortest_paths(g, dm);
 
   std::cout << "distances and parents:" << std::endl;
-  graph_traits <graph_t>::vertex_iterator vi, vend;
-  for (boost::tie(vi, vend) = vertices(g); vi != vend; ++vi) {
+  
+  for (tie(vi, vend) = vertices(g); vi != vend; ++vi) {
     std::cout << "distance(" << indexmap[*vi] << ") = " << d[*vi] << ", ";
     std::cout << "parent(" << indexmap[*vi] << ") = " << indexmap[p[*vi]] << std::endl;
   }
   std::cout << std::endl;
 
   std::cout << "distances:" << std::endl;
-  for (boost::tie(vi, vend) = vertices(g); vi != vend; ++vi) {
+  for (tie(vi, vend) = vertices(g); vi != vend; ++vi) {
     std::cout << "distances(" << indexmap[*vi] << ") =";
 
     graph_traits <graph_t>::vertex_iterator vix, vendx;
-    for (boost::tie(vix, vendx) = vertices(g); vix != vendx; ++vix) {
+    for (tie(vix, vendx) = vertices(g); vix != vendx; ++vix) {
       std::cout << " " << dm[*vi][*vix];
     }
     std::cout << std::endl;
   }
+  std::cout << std::endl;*/
 
-  breadth_first_search(g, s);
-  //depth_first_search(g, s);
+  breadth_first_search(g, s, color_map(cm));
+  depth_first_search(g, root_vertex(s).color_map(cm));
+
+  std::cout << "visited:" << std::endl;
+  for (tie(vi, vend) = vertices(g); vi != vend; ++vi) {
+    std::cout << "visited(" << indexmap[*vi] << ") = " << cm[*vi] << std::endl;
+  }
+  std::cout << std::endl;
 
   return EXIT_SUCCESS;
 }
